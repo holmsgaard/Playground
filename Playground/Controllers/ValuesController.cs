3101436407cf9114300d2da3b63d524406d8f963
Peter Holmsgaard <pjh@jcd.dk>
2015-06-26 12:45:30 +0200

    init

---
+++ Playground/Controllers/ValuesController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Device.Location;
+
+namespace Playground.Controllers
+{
+    public class ValuesController : ApiController
+    {
+        public HttpResponseMessage GetDistance(double lat, double lng, double latTo, double lngTo) {
+
+            HttpResponseMessage response = new HttpResponseMessage();
+            try
+            {
+                GeoCoordinate loc1 = new GeoCoordinate(lat, lng);
+                GeoCoordinate loc2 = new GeoCoordinate(latTo, lngTo); // Systemvej 12
+
+                double distance = loc1.GetDistanceTo(loc2) / 1000;
+
+                response.StatusCode = HttpStatusCode.OK;
+                response.Content = new StringContent(distance.ToString());
+            }
+            catch (Exception ex) {
+                response.StatusCode = HttpStatusCode.InternalServerError;
+                response.Content = new StringContent("Duer ik!");
+            }
+            
+            return response;
+        }
+    }
+}
---
+++ Playground/Scripts/controllers.js
@@ -0,0 +1,63 @@
+﻿var locApp = angular.module('locApp', []);
+
+locApp.controller('HomeController', function ($scope, locationService) {
+    $scope.wat = ['1'];
+    console.log($scope.wat);
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition)
+    }
+    else {
+        $scope.position = null;
+    }
+
+    function showPosition(position) {
+        console.log('position', position);
+        $scope.position = {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude
+        }
+    }
+
+    $scope.lat = 57.070025;
+    $scope.lng = 9.545024;
+
+    $scope.showPosition = function (position) {
+        console.log('position', position);
+        $scope.position = {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude
+        }
+    }
+
+    $scope.getDistance = function () {
+        console.log(
+            $scope.position,
+            [$scope.lat, $scope.lng]
+        );
+
+        
+        if ($scope.position != null) {
+            locationService.getDistance($scope.lat, $scope.lng, $scope.position.lat, $scope.position.lng).then(function (response) {
+                $scope.distance = response.data;
+            });
+        }
+        else {
+            alert('Ingen position');
+        }
+
+    }
+
+    
+
+}).service('locationService', function ($http) {
+    var locationService = {
+        getDistance: function (lat, lng, latTo, lngTo) {
+            var promise = $http.get('/api/values/GetDistance?lat=' + lat + '&lng=' + lng + '&latTo=' + latTo + '&lngTo=' + lngTo).then(function (response) {
+                console.log(response);
+                return response;
+            });
+            return promise;
+        }
+    }
+    return locationService;
+});
